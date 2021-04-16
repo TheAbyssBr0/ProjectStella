46dc7b65a1fa0b23d57c9be148050bee87ce8cd8
shakib <shr382@usask.ca>
2021-04-16 11:27:49 +0600

    Added driver class

---
+++ src/main/java/git/stella/model/driver/Driver.java
@@ -0,0 +1,21 @@
+package git.stella.model.driver;
+
+import git.stella.model.generator.Generator;
+import git.stella.model.user.User;
+import git.stella.model.utils.KeyContainer;
+
+public class Driver {
+    public static void main(String[] args) {
+        // inline tester
+        String username = "Security maniac";
+        String password = "1234password";
+        String service = "4chan";
+        KeyContainer keyContainer = new KeyContainer(true, true, true, true);
+        int len = 16;
+        int num = 1;
+
+        User user = new User(username, password);
+        Generator generator = new Generator(user);
+        System.out.println("Password generated is: " + generator.getPassword(service, keyContainer, len, num));
+    }
+}
